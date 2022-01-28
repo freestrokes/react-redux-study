f1e00015b6a29eb87d86b6095790b1211262f415
freestrokes <freestrokes.hk@gmail.com>
2022-01-28 09:21:38 +0900

    [ui] #master Modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import RecoilCounter from './screens/RecoilCounter';
-import RecoilTodo from './screens/RecoilTodo';
+import React, {useState} from 'react';
+import { userSelector, usersSelector } from './states/userState';
+import {useRecoilState, useRecoilValue, useRecoilValueLoadable} from 'recoil';
+// import RecoilCounter from './screens/RecoilCounter';
+// import RecoilTodo from './screens/RecoilTodo';
 // import {useAppDispatch} from './store/config';
 // import {UserService} from './services/userService';
 // import Counter from './screens/Counter';
@@ -8,57 +10,76 @@ import RecoilTodo from './screens/RecoilTodo';
 
 function App() {
 
+	// TODO
+	// redux toolkit
 	// const dispatch = useAppDispatch();
+	// const [recoilUser, setRecoilUser] = useRecoilState(recoilUserSelector);
+
+	// TODO
+	// RecoilValue
+	// const [userId, setUserId] = useState(0);
+	// const user = useRecoilValue(userSelector(userId));
+
+	// TODO
+	// waitForAll/waitForNone
+	const usersLoadable = useRecoilValueLoadable(usersSelector);
+	let users = '';
+	switch (usersLoadable.state) {
+		case 'hasValue':
+			users = JSON.stringify(usersLoadable.contents);
+			break;
+		case 'hasError':
+			users = usersLoadable.contents.message;
+			break;
+		case 'loading':
+			users = 'Loading...';
+			break;
+		default:
+			users = 'Loading...';
+	};
+
+	// TODO
+	// RecoilValueLoadable
+	// const userLoadable = useRecoilValueLoadable(userSelector(userId));
+	// let user = '';
+	// switch (userLoadable.state) {
+	// 	case 'hasValue':
+	// 		user = JSON.stringify(userLoadable.contents);
+	// 		break;
+	// 	case 'hasError':
+	// 		user = userLoadable.contents.message;
+	// 		break;
+	// 	case 'loading':
+	// 		user = 'Loading...';
+	// 		break;
+	// 	default:
+	// 		user = 'Loading...';
+	// };
 
 	/**
-	 * API TEST (GET)
+	 * Get User
 	 */
-	// const getTest = async () => {
-	// 	// @ts-ignore
-	// 	dispatch(UserService.getUser('1'));
+	// const getUser = () => {
+	// 	setUserId(userId + 1);
 	// };
 
 	return (
 		<>
-			<RecoilCounter />
-			<hr />
-			<RecoilTodo />
+			{/*<RecoilCounter />*/}
+			{/*<hr />*/}
+			{/*<RecoilTodo />*/}
 			{/*<Counter />*/}
 			{/*<hr />*/}
 			{/*<Todo />*/}
 			{/*<p/>*/}
-			{/*<button onClick={getTest}>[API TEST (GET)]</button>*/}
+
+			{/*<button onClick={getUser}>[ GET USER ]</button>*/}
+			{/*<p/>*/}
+			{/*{user}*/}
+
+			{users}
 		</>
 	);
 }
 
 export default App;
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import Hello from './Hello';
-
-// function App() {
-//   return (
-//     <div className="App">
-// 	    {/*<div>*/}
-// 		    {/*<Hello />*/}
-// 	    {/*</div>*/}
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
