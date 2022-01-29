e28c0369aa8ef2a36f61d4e70ebab37f32e33162
freestrokes <freestrokes.hk@gmail.com>
2022-01-29 20:08:53 +0900

    [ui] #master Modified redux files.

---
+++ src/App.redux.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './screens/redux/Counter';
+import Todo from './screens/redux/Todo';
+
+function App() {
+	return (
+		<>
+			<Counter />
+			<hr />
+			<Todo />
+		</>
+	);
+}
+
+export default App;
