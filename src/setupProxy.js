096c9f7f5d0cd21507f6d60ead771b1dd678a71a
freestrokes <freestrokes.hk@gmail.com>
2022-03-15 17:51:42 +0900

    [ui] update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -12,3 +12,25 @@ module.exports = (app) => {
 		)
 	);
 }
+
+
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = (app) => {
+	app.use(
+		createProxyMiddleware(
+			['/test', '/users'],
+			{
+				target: process.env.REACT_APP_API_URL,
+				changeOrigin: true,
+				router: {
+					'/test': process.env.REACT_APP_API_URL,
+					'/users': process.env.REACT_APP_API_URL,
+				},
+				pathRewrite: {
+					'^/test': '',
+				}
+			}
+		)
+	);
+}
